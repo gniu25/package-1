6ba9fb8ea9028278bb0fcc3a800cbf87b12812fa
Hendrik Hilleckes <hendrik.hilleckes@leogistics.corp>
2017-09-08 23:18:50 +0200

    added test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,10 @@
 # Sample Test passing with nose and pytest
 
-def test_pass():
+from .context import MyClass
+
+def test_sample():
     assert True, "dummy sample test"
+
+def test_get_5():
+    myclass = MyClass()
+    assert myclass.get_5() == 5, "MyClass get_5 returns 5"
